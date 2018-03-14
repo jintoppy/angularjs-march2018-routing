ad4473c89ba0ddca23dec2196aee819c5a7fed6d
Jinto Jose <jintoppy@gmail.com>
2018-03-14 10:24:18 +0530

    adding ui router

--- app.js
+++ app.js
@@ -1,2 +1,19 @@
-angular.module('myapp', []);
+var myapp = angular.module('myapp', ['ui.router']);
+
+myapp.config(['$stateProvider', 
+    function($stateProvider){
+
+        $stateProvider.state({
+            name: 'home',
+            url: '/home',
+            template: '<div>Home page</div>'
+        });
+
+        $stateProvider.state({
+            name: 'about',
+            url: '/about',
+            template: '<div>About page</div>'
+        });
+
+    }]);
 
